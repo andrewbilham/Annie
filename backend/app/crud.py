f32ffd4dfb58b80e8952958d8da790ebb21bdd46
andrewbilham <andrewbilham@me.com>
2024-06-13 20:51:18 +0100

    test

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -52,7 +52,6 @@ def create_item(*, session: Session, item_in: ItemCreate, owner_id: int) -> Item
     session.refresh(db_item)
     return db_item
 
-
 def create_source(*, session: Session, source_in: SourceCreate, owner_id: int) -> Source:
     db_source = Source.model_validate(source_in, update={"owner_id": owner_id})
     session.add(db_source)
@@ -66,5 +65,59 @@ def create_supplier(*, session: Session, supplier_in: SupplierCreate, owner_id: 
     session.commit()
     session.refresh(db_supplier)
     return db_supplier
-    
 
+def create_claim(*, session: Session, claim_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_claim = Supplier.model_validate(claim_in, update={"owner_id": owner_id})
+    session.add(db_claim)
+    session.commit()
+    session.refresh(db_claim)
+    return db_claim
+
+def create_authority(*, session: Session, authority_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_authority = Supplier.model_validate(authority_in, update={"owner_id": owner_id})
+    session.add(db_authority)
+    session.commit()
+    session.refresh(db_authority)
+    return db_authority    
+    
+def create_circs_group(*, session: Session, circs_group_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_circs_group = Supplier.model_validate(circs_group_in, update={"owner_id": owner_id})
+    session.add(db_circs_group)
+    session.commit()
+    session.refresh(db_circs_group)
+    return db_circs_group
+
+def create_veh_group(*, session: Session, veh_group_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_veh_group = Supplier.model_validate(veh_group_in, update={"owner_id": owner_id})
+    session.add(db_veh_group)
+    session.commit()
+    session.refresh(db_veh_group)
+    return db_veh_group
+
+def create_referrals_allocation(*, session: Session, referrals_allocation_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_referrals_allocation = Supplier.model_validate(referrals_allocation_in, update={"owner_id": owner_id})
+    session.add(db_referrals_allocation)
+    session.commit()
+    session.refresh(db_referrals_allocation)
+    return db_referrals_allocation
+
+def create_source_rate(*, session: Session, source_rate_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_source_rate = Supplier.model_validate(source_rate_in, update={"owner_id": owner_id})
+    session.add(db_source_rate)
+    session.commit()
+    session.refresh(db_source_rate)
+    return db_source_rate
+
+def create_supplier_rate(*, session: Session, supplier_rate_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_supplier_rate = Supplier.model_validate(supplier_rate_in, update={"owner_id": owner_id})
+    session.add(db_supplier_rate)
+    session.commit()
+    session.refresh(db_supplier_rate)
+    return db_supplier_rate
+
+def create_claim(*, session: Session, claim_in: SupplierCreate, owner_id: int) -> Supplier:
+    db_claim = Supplier.model_validate(claim_in, update={"owner_id": owner_id})
+    session.add(db_claim)
+    session.commit()
+    session.refresh(db_claim)
+    return db_claim
